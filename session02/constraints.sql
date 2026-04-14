12e4ea1d18947e1a2faea48607979cd547918495
C4EJS100-TrinhQuocHai <trinhquochai12@gmail.com>
2026-04-14 09:44:20 +0700

    Add

--- session02/constraints.sql
+++ session02/constraints.sql
@@ -4,6 +4,10 @@
     - Nhất quán dữ, tránh dư thừa, dữ liệu rác 
     1. PRIMARY KEY : Khóa chính ( không trùng, không rỗng)
     2. UNIQUE      : duy nhất không trùng dữ liệu
+    3. NOT NULL    : không được để trống
+    4. CHECK       : kiểm tra thêm điều kiện 
+    5. DEFAULT     : nếu không điền sẽ lấy giá trị mặc định 
+    6. Khóa ngoại  : Liên kết dữ liệu giữa các bảng
 */
 USE session02;
 CREATE TABLE person(
@@ -11,4 +15,21 @@ CREATE TABLE person(
     fullname varchar(50)
 );
 ALTER TABLE person
-ADD email varchar(50) unique
+ADD email varchar(50) unique ;
+ALTER TABLE person
+ADD age int CHECK(age>=18); 
+ALTER TABLE person
+ADD address varchar(50) DEFAULT 'HN';
+
+-- bảng cha _ danh mục sản phẩm
+CREATE TABLE category(
+	categoryId INT PRIMARY KEY,
+    categoryName varchar(50)
+);
+-- bảng con _ sản phẩm
+CREATE TABLE products(
+	productId INT PRIMARY KEY,
+    productName varchar (50),
+    categoryId INT,
+    foreign key (categoryId) references  category(categoryId)
+)
